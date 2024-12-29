10dc49d28d216c59bff5d0bdd85a266b49f86127
coderlearner <codelearner@gmail.com>
2024-12-29 12:26:44 +0530

    fg

--- backend/routes/emergency.js
+++ backend/routes/emergency.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const turf = require('@turf/turf');
-const officerRegister = require('../models/OfficerRegister.js');
+const officerRegister = require('../models/officerRegister.js');
 require('dotenv').config();
 
 // Get LocationIQ API Key from environment variables
