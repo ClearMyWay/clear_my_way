f4275ff70d14ae6a589eee42c8d0b8ce8957bff1
coderlearner <codelearner@gmail.com>
2024-12-29 16:34:01 +0530

    otp

--- backend/models/officerRegister.js
+++ backend/models/officerRegister.js
@@ -5,8 +5,8 @@ const OfficerRegSchema = new mongoose.Schema({
     mobileNumber: { type: String, required: true, unique: true },
     Password: { type: String, required: true,  unique: true },
     socketId: { type: String }, 
-    lat: { type: number },
-    lng: { type: number },
+    lat: { type: Number },
+    lng: { type: Number },
     lastUpdated: { type: Date, default: Date.now },
 })
 
